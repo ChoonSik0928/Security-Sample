74392da10baa54ebd42c3bd8b5e32bfafe9118f7
choonsik0928 <choonsik0928@gmail.com>
2020-03-31 19:35:31 +0900

    simple crypto 샘플 추가

--- app/src/main/java/com/choonsik/security_sample/ui/simple_crypt/SimpleCryptViewModel.kt
+++ app/src/main/java/com/choonsik/security_sample/ui/simple_crypt/SimpleCryptViewModel.kt
@@ -1,12 +1,34 @@
 package com.choonsik.security_sample.ui.simple_crypt
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.choonsik.security_sample.base.BaseViewModel
+import com.choonsik.security_sample.R
+import com.choonsik.security_sample.util.crypt.CryptManager
 import javax.inject.Inject
 
 class SimpleCryptViewModel @Inject constructor() : ViewModel() {
+
     val editValue = MutableLiveData<String>()
 
+    val encryptedValue = MutableLiveData<String>()
+    val decryptedValue = MutableLiveData<String>()
 
+    fun onButtonClick(view: View) {
+        when (view.id) {
+            R.id.btn_encrypt -> {
+                val plainText = editValue.value ?: return
+                encryptedValue.value = CryptManager.encryptPlainText(KEY_SIMPLE, plainText)
+            }
+
+            R.id.btn_decrypt -> {
+                val encryptedText = encryptedValue.value ?: return
+                decryptedValue.value = CryptManager.decryptPlainText(KEY_SIMPLE, encryptedText)
+            }
+        }
+    }
+
+    companion object {
+        const val KEY_SIMPLE = "KEY_SIMPLE"
+    }
 }
