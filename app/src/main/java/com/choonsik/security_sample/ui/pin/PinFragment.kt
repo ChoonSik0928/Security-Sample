15b16bfc3606e583bbbc5a1f111d9cfab9d6e474
choonsik0928 <choonsik0928@gmail.com>
2020-04-02 20:09:13 +0900

    PinFragment listener 추가 viewModel 수정

--- app/src/main/java/com/choonsik/security_sample/ui/pin/PinFragment.kt
+++ app/src/main/java/com/choonsik/security_sample/ui/pin/PinFragment.kt
@@ -5,6 +5,9 @@ import android.view.View
 import com.choonsik.security_sample.R
 import com.choonsik.security_sample.base.BaseFragment
 import com.choonsik.security_sample.databinding.FragmentPinBinding
+import com.choonsik.security_sample.widget.pin.`interface`.KeyboardClickListener
+import com.choonsik.security_sample.widget.pin.keyboard.PinKey
+import kotlinx.android.synthetic.main.fragment_pin.*
 
 class PinFragment : BaseFragment<PinViewModel, FragmentPinBinding>(
     R.layout.fragment_pin, PinViewModel::class
@@ -12,6 +15,29 @@ class PinFragment : BaseFragment<PinViewModel, FragmentPinBinding>(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        pin_keyboard.setKeyboardClickListener(object :KeyboardClickListener{
+            override fun onKeyClick(key: PinKey) {
+                viewModel.onKeyClick(key)
+            }
+        })
+//        var result = arrayListOf<String>()
+//        pin_keyboard.setKeyboardClickListener(object : KeyboardClickListener {
+//            override fun onKeyClick(key: PinKey) {
+//
+//                when (key) {
+//                    is PinKey.BackKey -> {
+//                        if(result.isNotEmpty()){
+//                            result.removeAt(result.lastIndex)
+//                        }
+//                    }
+//
+//                    is PinKey.Num -> {
+//                        result.add(PinKey.getString(key))
+//                    }
+//                }
+//
+//                tv_result.text = result.toString()
+//            }
+//        })
     }
 }
--- app/src/main/java/com/choonsik/security_sample/ui/pin/PinViewModel.kt
+++ app/src/main/java/com/choonsik/security_sample/ui/pin/PinViewModel.kt
@@ -1,8 +1,36 @@
 package com.choonsik.security_sample.ui.pin
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.choonsik.security_sample.widget.pin.keyboard.PinKey
 import javax.inject.Inject
 
 class PinViewModel @Inject constructor() : ViewModel() {
 
+    val inputKey = MutableLiveData<String>()
+    private val _registrationKeys = arrayListOf<PinKey>()
+    private val _inputKeys = arrayListOf<PinKey>()
+    fun addPin() {
+
+    }
+
+    fun onKeyClick(key: PinKey) {
+        when (key) {
+            is PinKey.BackKey -> {
+
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun registrationKey(key: PinKey) {
+        _registrationKeys.add(key)
+    }
+
+    companion object {
+        private const val PIN_MAX_SIZE = 4
+    }
 }
